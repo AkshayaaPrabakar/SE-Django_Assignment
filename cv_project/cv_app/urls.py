4696038fc23780544fb48cc02e0f285c2ded279c
Sai <saiakshayaa2000@gmail.com>
2025-02-18 22:20:31 -0500

    Updated project

--- cv_project/cv_app/urls.py
+++ cv_project/cv_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path # type: ignore
+from django.urls import path  # type: ignore
 from .views import resume_view
 
 urlpatterns = [
+    path('', resume_view, name='home'),  # Set this as the default route
     path('resume/', resume_view, name='resume'),
 ]
